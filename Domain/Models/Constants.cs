9c330d3bb4c9c9653fcbc41dfd5ee22c0cbb7ff4
Maxim Paramonov <ncmail@ukr.net>
2020-08-28 23:01:23 +0300

    phone pattern

--- Domain/Models/Constants.cs
+++ Domain/Models/Constants.cs
@@ -4,12 +4,11 @@
     {
 #if DEBUG
         public const string DefaultConnection = "Server=DESKTOP-JPQ21PS;Database=TestBd;Trusted_Connection=True;MultipleActiveResultSets=true";
-       
+        
 #else
         //.102
         public const string DefaultConnection =
             "Server=SP-VL;Database=TestBd;Persist Security Info=True;User ID=xxx;Password=xxx";
-
 #endif
     }
 }
